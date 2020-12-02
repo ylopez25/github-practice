0a3a6cd5e304b93e32195e599923212ffa48467a
Jarrett Harris <Jarrettharris@pursuit.org>
2020-12-02 16:28:46 -0500

    change to index file

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 GitHub keeps meticulous track of all the changes made to a project
+
+"In order to do so, you save these changes by commiting them. In order to commit them, you have to first tell Git to stage them."
